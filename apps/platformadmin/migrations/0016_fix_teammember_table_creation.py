e872ffe000d75ed0a42f61bc8e8cd8675b19ab89
aditya2023 <adityamishraniet2023@gmail.com>
2026-02-11 09:54:01 +0530

    f12

---
+++ apps/platformadmin/migrations/0016_fix_teammember_table_creation.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('platformadmin', '0015_merge_20260211_0940'),
+    ]
+
+    operations = [
+    ]
