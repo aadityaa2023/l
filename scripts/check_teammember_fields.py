bc1554922db87a07ddb4d910ec3f3518d968050b
aditya2023 <adityamishraniet2023@gmail.com>
2026-03-01 12:19:58 +0530

    fixed

--- scripts/check_teammember_fields.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leq.settings')
-django.setup()
-
-from apps.platformadmin.models import TeamMember
-
-print([f.name for f in TeamMember._meta.get_fields()])
